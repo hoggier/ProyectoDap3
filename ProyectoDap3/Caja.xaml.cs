99c5ae738a821e795e8a859f1d0b43490ca2121b
Hoggier Cuellar Hurtado <hcrcuellar13@gmail.com>
2012-09-26 01:52:53 -0400

    interfaz inicial,modelo de datos 10 tablas

---
+++ ProyectoDap3/Caja.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoDap3
+{
+    /// <summary>
+    /// Lógica de interacción para Caja.xaml
+    /// </summary>
+    public partial class Caja : Window
+    {
+        public Caja()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ProyectoDap3/Ingresar.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoDap3
+{
+    /// <summary>
+    /// Lógica de interacción para Ingresar.xaml
+    /// </summary>
+    public partial class Ingresar : Window
+    {
+        public Ingresar()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnIngresar_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
