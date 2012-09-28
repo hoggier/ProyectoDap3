1b4b74d144a58f27bbee9c006599c20c4e0123c7
Hoggier Cuellar Hurtado <hcrcuellar13@gmail.com>
2012-09-28 16:38:27 -0400

    proyecto funcionando con ojo:solo agregar, no muestra ni modifica

--- ProyectoDap3/Cajero.xaml.cs
+++ ProyectoDap3/Cajero.xaml.cs
@@ -26,6 +26,8 @@ namespace ProyectoDap3
         dbCajaEntities objContexto = new dbCajaEntities();
 
         #region "Metodos"
+        IngresoCaja objIngcaja = new IngresoCaja();
+        
         private void abrirCaja()
         {
             Caja objCaja = new Caja();
@@ -78,6 +80,55 @@ namespace ProyectoDap3
                 Clases.CCajero.cajeroid = x.idUsuario;
             }
         }
+
+        public void almacenarCxC()
+        {
+            if (!Clases.CCajero.cxCguardada)
+            {
+                CxC objCuenta = new CxC();
+                objCuenta.estado = "En Espera";
+                objCuenta.fechahora = DateTime.Now;
+                objCuenta.montoPagar = decimal.Parse(txbMontoCuenta.Text);
+                Clases.CCajero.montopagar = objCuenta.montoPagar;
+                Clases.CCajero.saldo = objCuenta.montoPagar;
+                txbMontoCuenta.IsReadOnly = true;
+                Clases.CCajero.cxCguardada = true;
+                objContexto.AddToCxCs(objCuenta);
+                objContexto.SaveChanges();
+            }
+        }
+
+        public void terminarPago()
+        {
+            var objCxc = from CxC c in objContexto.CxCs
+                         where c.estado == "En Espera"
+                         select c;
+            foreach (CxC x in objCxc)
+            {
+                Clases.CCajero.CxCid = x.idCxC;
+                x.estado = "Pagado";
+            }
+            objContexto.SaveChanges();
+
+            objIngcaja.idCxC = Clases.CCajero.CxCid;
+            objIngcaja.idCaja = Clases.CCajero.cajaid;
+            lblSaldo.Content = "0";
+            lblCambio.Content = (Clases.CCajero.saldo + (Clases.CCajero.saldo * -2)).ToString();
+            objContexto.AddToIngresoCajas(objIngcaja);
+            objContexto.SaveChanges();
+
+            txbMontoCuenta.Text = "";
+            txbMontoIngreso.Text = "";
+            Clases.CCajero.cxCguardada = false;
+            Clases.CCajero.montopagar = 0;
+            Clases.CCajero.montoingresado = 0;
+            Clases.CCajero.saldo = 0;
+            Clases.CCajero.cambio = 0;
+
+            txbMontoCuenta.IsReadOnly = false;
+            objIngcaja = new IngresoCaja();
+            MessageBox.Show("Se termino de realizar el pago");
+        }
         #endregion
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -119,5 +170,153 @@ namespace ProyectoDap3
                 MessageBox.Show(a.Message);
             }            
         }
+
+        private void btnRetirarDotacion_Click(object sender, RoutedEventArgs e)
+        {
+            var objDotacion = from Dotacion d in objContexto.Dotacions
+                              where d.caja == Clases.CCajero.cajaid
+                              select d;
+            foreach (Dotacion x in objDotacion)
+            {
+                lblRetiroBss.Content = x.efectivo.ToString();
+                x.entregado = "S";
+            }
+            objContexto.SaveChanges();
+            detalles.Visibility = Visibility.Visible;
+        }
+
+        private void btnRetirar_Click(object sender, RoutedEventArgs e)
+        {
+            EgresoCaja objEgreso = new EgresoCaja();
+            objEgreso.idCaja = Clases.CCajero.cajaid;
+            objEgreso.MontoEgreso = 0;
+            objContexto.AddToEgresoCajas(objEgreso);
+            objContexto.SaveChanges();
+            var objE = from EgresoCaja ec in objContexto.EgresoCajas
+                       select ec;
+            foreach (EgresoCaja x in objE)
+            {
+                Clases.CCajero.egresocajaid = x.idEgreso;
+            }
+
+            DetallesEgreso objDetalle = new DetallesEgreso();
+            objDetalle.IdEgreso = Clases.CCajero.egresocajaid;
+            objDetalle.c200bs = int.Parse(txbbs200.Text);
+            objDetalle.c100bs = int.Parse(txbbs100.Text);
+            objDetalle.c50bs = int.Parse(txbbs50.Text);
+
+            Clases.CCajero.totalbs = (objDetalle.c200bs * 200) + (objDetalle.c100bs * 100) + (objDetalle.c50bs * 50);
+
+            objDetalle.c100dolares = int.Parse(txbdolar100.Text);
+            Clases.CCajero.totaldolares = objDetalle.c100dolares * 100;
+            objDetalle.c50dolares = int.Parse(txbdolar50.Text);
+            Clases.CCajero.totaldolares += (objDetalle.c50dolares * 50);
+            objDetalle.c20dolares = int.Parse(txbdolar20.Text);
+            Clases.CCajero.totaldolares += (objDetalle.c20dolares * 20);
+            objDetalle.c10dolares = int.Parse(txbdolar10.Text);
+            Clases.CCajero.totaldolares += (objDetalle.c10dolares * 10);
+            objDetalle.c5dolares = int.Parse(txbdolar5.Text);
+            Clases.CCajero.totaldolares += (objDetalle.c5dolares * 5);
+            objDetalle.c1dolares = int.Parse(txbdolar1.Text);
+            Clases.CCajero.totaldolares += (objDetalle.c1dolares * 1);
+            Clases.CCajero.totaldolaresenbs = Clases.CCajero.totaldolares * Clases.CTipoCambio.tcambio;
+            Clases.CCajero.totalretiro = Clases.CCajero.totaldolaresenbs + Clases.CCajero.totalbs;
+
+            objDetalle.TCambio = Clases.CTipoCambio.tcambio;
+
+            objContexto.AddToDetallesEgresoes(objDetalle);
+            objContexto.SaveChanges();
+
+            lblRetiroBss.Content = Clases.CCajero.totalbs.ToString();
+            lblRetirodolaress.Content = Clases.CCajero.totaldolares.ToString();
+            lblSusenBss.Content = Clases.CCajero.totaldolaresenbs.ToString();
+            lblTotalRetiros.Content = Clases.CCajero.totalretiro.ToString();
+
+            detalles.Visibility = Visibility.Visible;
+        }                        
+        
+        private void btnEfectivo_Click(object sender, RoutedEventArgs e)
+        {
+            almacenarCxC();
+            objIngcaja.bs = decimal.Parse(txbMontoIngreso.Text);
+            Clases.CCajero.saldo = Clases.CCajero.saldo - objIngcaja.bs;
+            if (Clases.CCajero.saldo > 0)
+            {
+                lblSaldo.Content = Clases.CCajero.saldo.ToString();
+                lblCambio.Content = "-" + Clases.CCajero.saldo.ToString();
+            }
+            else
+            {
+                terminarPago();
+            }
+        }
+
+        private void btnVisa_Click(object sender, RoutedEventArgs e)
+        {
+            almacenarCxC();
+            objIngcaja.visa = decimal.Parse(txbMontoIngreso.Text);
+            Clases.CCajero.saldo = Clases.CCajero.saldo - objIngcaja.visa;
+            if (Clases.CCajero.saldo > 0)
+            {
+                lblSaldo.Content = Clases.CCajero.saldo.ToString();
+                lblCambio.Content = "-" + Clases.CCajero.saldo.ToString();
+            }
+            else
+            {
+                terminarPago();
+            }
+        }
+
+        private void btnMaster_Click(object sender, RoutedEventArgs e)
+        {
+            almacenarCxC();
+            objIngcaja.master = decimal.Parse(txbMontoIngreso.Text);
+            Clases.CCajero.saldo = Clases.CCajero.saldo - objIngcaja.master;
+            if (Clases.CCajero.saldo > 0)
+            {
+                lblSaldo.Content = Clases.CCajero.saldo.ToString();
+                lblCambio.Content = "-" + Clases.CCajero.saldo.ToString();
+            }
+            else
+            {
+                terminarPago();
+            }
+        }
+
+        private void btnPromo_Click(object sender, RoutedEventArgs e)
+        {
+            almacenarCxC();
+            objIngcaja.promocion = decimal.Parse(txbMontoIngreso.Text);
+            Clases.CCajero.saldo = Clases.CCajero.saldo - objIngcaja.promocion;
+            if (Clases.CCajero.saldo > 0)
+            {
+                lblSaldo.Content = Clases.CCajero.saldo.ToString();
+                lblCambio.Content = "-" + Clases.CCajero.saldo.ToString();
+            }
+            else
+            {
+                terminarPago();
+            }
+        }
+
+        private void btnDolar_Click(object sender, RoutedEventArgs e)
+        {
+            almacenarCxC();
+            objIngcaja.dolares = decimal.Parse(txbMontoIngreso.Text);
+            objIngcaja.bolivianosTCambio = objIngcaja.dolares * Clases.CTipoCambio.tcambio;
+            objIngcaja.TCambio = Clases.CTipoCambio.tcambio;
+
+            Clases.CCajero.saldo = Clases.CCajero.saldo - objIngcaja.bolivianosTCambio;
+            if (Clases.CCajero.saldo > 0)
+            {
+                lblSaldo.Content = Clases.CCajero.saldo.ToString();
+                lblCambio.Content = "-" + Clases.CCajero.saldo.ToString();
+            }
+            else
+            {
+                terminarPago();
+            }
+        }
+
     }
 }
--- ProyectoDap3/Clases/CCajero.cs
+++ ProyectoDap3/Clases/CCajero.cs
@@ -11,6 +11,19 @@ namespace ProyectoDap3.Clases
         public static int cajeroid = 0;
         public static int cajaid = 0;
         public static int personaid = 0;
+        public static int egresocajaid = 0;
+        public static decimal totalbs = 0;
+        public static decimal totaldolares = 0;
+        public static decimal totaldolaresenbs = 0;
+        public static decimal totalretiro = 0;
+
+        public static int CxCid = 0;
+        public static decimal montopagar = 0;
+        public static decimal montoingresado = 0;
+        public static decimal saldo = 0;
+        public static decimal cambio = 0;
+
+        public static bool cxCguardada = false;
         public static bool cajaabierta = false;
     }
 }
